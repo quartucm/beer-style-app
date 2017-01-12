95255bee2cd571a38df1ec280534191386d2ba37
Marlena Quartuccia <marlena.quartuccia@publicisna.com>
2017-01-12 12:48:16 -0500

    Adding in history

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { Appbar, Button, Panel } from 'muicss/react';
+import { history } from '../../store/index';
 
 const Header = React.createClass({
 	render() {
 		return (
-			<div><h1>Ratings App</h1>
-				 <Link to='/beer'>
-		            Back
-		         </Link>
+
+			<div>			
+				<Appbar>
+					<h1>Ratings App</h1>
+					<button onClick={() => {history.goBack()}}>Back</button>
+				</Appbar>
 	         </div>
 		);
 	}
