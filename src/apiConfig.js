977ac301a9a48f607cf87d4c676671156f635a70
Marlena Quartuccia <marlena.quartuccia@publicisna.com>
2017-01-13 11:22:06 -0500

    Dynamic port

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -3,7 +3,7 @@ const apiConfig = {
 		'api' : 'http://localhost:8181'
 	},
 	'beer-style-app.herokuapp.com' : {
-		'api' : 'https://beer-style-app.herokuapp.com:8181'
+		'api' : `https://beer-style-app.herokuapp.com:${process.env.PORT}`
 	}
 
 }[window.location.hostname]
