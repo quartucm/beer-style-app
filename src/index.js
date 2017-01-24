cfb706c2c8f5ba88cf22331bc17810288168b380
Marlena <marlena@Lous-MBP.home>
2017-01-23 21:45:46 -0500

    Reverting universal attempt on master

--- src/index.js
+++ src/index.js
@@ -3,24 +3,9 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from 'store';
 import routes from 'routes';
-import { createBrowserHistory } from 'history';
-
-export const history = createBrowserHistory();
 
 
-const reduxRouterMiddleware = syncHistory(browserHistory);
-const myStore = store(window.__INITIAL_STATE__, reduxRouterMiddleware);
-
-// render(
-//   <Provider store={store}>{routes}</Provider>,
-//   document.getElementById('react')
-// );
-
-match({ routes, location }, () => {
-  render(
-    <Provider store={ myStore }>
-      <Router routes={ routes } history={ history }/>
-    </Provider>,
-    document.getElementById('react')
-  );
-});
+render(
+  <Provider store={store}>{routes}</Provider>,
+  document.getElementById('react')
+);
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 // Create final store using all reducers and applying middleware
-// import { createBrowserHistory } from 'history';
+import { createBrowserHistory } from 'history';
 // Redux utility functions
 import { compose, createStore, combineReducers, applyMiddleware } from 'redux';
 import { routerMiddleware, connectRouter } from 'connected-react-router';
@@ -10,7 +10,7 @@ import * as reducers from '../reducers';
 // Configure reducer to store state at state.router
 // You can store it elsewhere by specifying a custom `routerStateSelector`
 // in the store enhancer below
-//export const history = createBrowserHistory();
+export const history = createBrowserHistory();
 const reducer = combineReducers({ ...reducers });
 
 const store = compose(
